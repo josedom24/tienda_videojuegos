61b3ad57f46eab95dccabd1d2a9ced3c02c2c94c
josedom24 <josedom24@gmail.com>
2017-08-17 11:12:02 +0200

    Compatible con mysql

--- aplicacion/config.py
+++ aplicacion/config.py
@@ -4,6 +4,7 @@ SECRET_KEY = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 PWD = os.path.abspath(os.curdir)
 
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}/dbase.db'.format(PWD)
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///{}/dbase.db'.format(PWD)
+SQLALCHEMY_DATABASE_URI = 'mysql://ususario:usuario@localhost/tienda'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
