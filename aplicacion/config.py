f8b3004f5a90570fe3838f79a5dfb336d9bddbd7
josedom24 <josedom24@gmail.com>
2017-08-21 13:25:49 +0200

    pythonanywhere

--- aplicacion/config.py
+++ aplicacion/config.py
@@ -5,6 +5,6 @@ PWD = os.path.abspath(os.curdir)
 
 DEBUG = True
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///{}/dbase.db'.format(PWD)
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://usuario:usuario@localhost/tienda'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://josedom24:usuario1234@josedom24.mysql.pythonanywhere-services.com/josedom24$tienda'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
